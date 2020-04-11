e5774905b1aaa3d6e94b8df057fe320a052f6009
Kaiserlucas <32715218+Kaiserlucas@users.noreply.github.com>
2020-04-11 16:44:09 +0200

    Challenge 3

---
+++ src/filepersistence/SensorDataStorage.java
@@ -0,0 +1,58 @@
+package filepersistence;
+
+import java.io.*;
+
+/**
+ * We assume: Each sensor gets its own storage engine. There wont be a parameter
+ * sensor name.
+ */
+public interface SensorDataStorage {
+
+    /**
+     * This method can be called by a sensor to save a data set.
+     * @param time UNIX time when measurement took place
+     * @param values sensor data
+     * @throws IOException if something unexpected happened. Insufficient right, medium broken, offline..
+     */
+    void saveData(long time, float[] values) throws IOException;
+
+    /**
+     * This method returns the timestamp of a data set specified by the parameter
+     * @param dataset number of the data set you want the values of
+     * @return timestamp of the specific data set
+     * @throws IllegalArgumentException if the dataset parameter is negative or bigger than the number of saved sets
+     */
+    long getTimestamp(int dataset) throws IllegalArgumentException;
+
+    /**
+     * This method returns the temperature values of a data set specified by the parameter
+     * @param dataset number of the data set you want the values of
+     * @return array with all found values of the specific data set
+     * @throws IllegalArgumentException if the dataset parameter is negative or bigger than the number of saved sets
+     */
+    float[] getValues(int dataset) throws IllegalArgumentException;
+
+    /**
+     * This method reads the current number of saved data sets from the size.txt file
+     * @return current amount of data sets
+     */
+    int getSize();
+
+    /**
+     * This method writes the amount of data sets into the size.txt file
+     * @param size amount of data sets currently saved
+     * @throws IllegalArgumentException if a negative size is passed as an argument
+     */
+    void writeSize(int size) throws IllegalArgumentException;
+
+    /**
+     * This method prints all data in data.txt to System.out
+     */
+    void printAllData();
+
+    /**
+     * This method deletes all sets stored in data.txt and resets size.txt to 0
+     */
+    void deleteAllData();
+
+}
--- src/filepersistence/WriteAndReadDataSet.java
+++ src/filepersistence/WriteAndReadDataSet.java
@@ -2,7 +2,7 @@ package filepersistence;
 
 import java.io.*;
 
-public class WriteAndReadDataSet{
+public class WriteAndReadDataSet implements SensorDataStorage{
     public static void main(String[] args) {
         // three example data sets
         String sensorName = "MyGoodOldSensor"; // does not change
@@ -33,75 +33,24 @@ public class WriteAndReadDataSet{
 
         WriteAndReadDataSet set = new WriteAndReadDataSet();
 
-        // write three data set into a file
-        OutputStream os = null;
-        try {
-            String filename = "data.txt";
-            os = new FileOutputStream(filename);
-
-        } catch (FileNotFoundException ex) {
-            System.err.println("couldn’t open file - fatal");
-            System.exit(0);
-
-        }
-
         /*
-         Writes all available data sets in the following format:
-
-         int number of data sets
-
-         then for each data set:
-
-         int number of characters in sensor name
-         string sensor name
-         long timestamp
-         int number of temperature values in the current data set
-         float temperature values
+         delete all previously saved data before adding the test data sets
+         (Just for cleaner looking testing. It would work without deleting old data beforehand as well
+         but the file would get very large very quickly by running this program repeatedly)
          */
-        set.writeInt(os, timeStamps.length);
-        for(int i = 0; i < timeStamps.length; i++) {
-            set.writeInt(os, sensorName.length());
-            set.writeString(os, sensorName);
-            set.writeLong(os,timeStamps[i]);
-            float[] tempArray = values[i];
-            set.writeInt(os, tempArray.length);
-            for(int j = 0; j < tempArray.length; j++) {
-                set.writeFloat(os, tempArray[j]);
-            }
-        }
+        set.deleteAllData();
 
-        // read data from file and print to System.out
-        InputStream is = null;
+        // Then saves all three test data sets in data.txt and prints them out.
         try {
-            String filename = "data.txt";
-            is = new FileInputStream(filename);
-
-        } catch (FileNotFoundException ex) {
-            System.err.println("couldn’t open file - fatal");
-            System.exit(0);
-
+            set.saveData(timeStamps[0], values[0]);
+            set.saveData(timeStamps[1], values[1]);
+            set.saveData(timeStamps[2], values[2]);
+        } catch (Exception ex) {
+            System.out.print(ex.getMessage()+" Data set could not be saved.");
         }
 
+        set.printAllData();
 
-        int dataSets = set.readInt(is);
-        for(int i = 0; i < dataSets; i++) {
-            System.out.println("Data set "+(i+1)+":");
-            int nameLength = set.readInt(is);
-            System.out.print("Sensor name: ");
-            for (int j = 0; j < nameLength; j++) {
-                System.out.print(set.readChar(is));
-            }
-            System.out.println("");
-
-            System.out.println("Timestamp: "+set.readLong(is));
-
-            int temperatureValues = set.readInt(is);
-            for(int j = 0; j < temperatureValues; j++) {
-               System.out.println("Value "+(j+1)+": "+set.readFloat(is));
-            }
-
-            System.out.println("");
-        }
 
     }
 
@@ -110,19 +59,8 @@ public class WriteAndReadDataSet{
     A couple of Methods that take inputs and write it to the file bound to the FileOutputStream.
     Takes an existing OutputStream and the variable you write to the file as parameters.
      */
-    public void writeString(OutputStream pos, String text) {
-        DataOutputStream dos = new DataOutputStream(pos);
 
-        try {
-            dos.writeChars(text);
-        } catch (IOException ex) {
-            System.err.println("couldn’t write data (fatal)");
-            System.exit(0);
-        }
-
-    }
-
-    public void writeInt(OutputStream pos, int value) {
+    private void writeInt(OutputStream pos, int value) {
         DataOutputStream dos = new DataOutputStream(pos);
 
         try {
@@ -134,7 +72,7 @@ public class WriteAndReadDataSet{
 
     }
 
-    public void writeLong(OutputStream pos, long value) {
+    private void writeLong(OutputStream pos, long value) {
         DataOutputStream dos = new DataOutputStream(pos);
 
         try {
@@ -146,7 +84,7 @@ public class WriteAndReadDataSet{
 
     }
 
-    public void writeFloat(OutputStream pos, float value) {
+    private void writeFloat(OutputStream pos, float value) {
         DataOutputStream dos = new DataOutputStream(pos);
 
         try {
@@ -164,7 +102,7 @@ public class WriteAndReadDataSet{
     Takes and InputStream to read from as a parameter and returns the data type specified in the method name.
      */
 
-    public float readFloat(InputStream pis) {
+    private float readFloat(InputStream pis) {
         DataInputStream dis = new DataInputStream(pis);
         float readValue = -1;
         try {
@@ -177,20 +115,7 @@ public class WriteAndReadDataSet{
         return readValue;
     }
 
-    public char readChar(InputStream pis) {
-        DataInputStream dis = new DataInputStream(pis);
-        char readValue = 'a';
-        try {
-            readValue = dis.readChar();
-        } catch (IOException ex) {
-            System.err.println("couldn’t read data (fatal). File seems to be corrupted.");
-            System.exit(0);
-        }
-
-        return readValue;
-    }
-
-    public long readLong(InputStream pis) {
+    private long readLong(InputStream pis) {
         DataInputStream dis = new DataInputStream(pis);
         long readValue = -1;
         try {
@@ -203,7 +128,7 @@ public class WriteAndReadDataSet{
         return readValue;
     }
 
-    public int readInt(InputStream pis) {
+    private int readInt(InputStream pis) {
         DataInputStream dis = new DataInputStream(pis);
         int readValue = -1;
         try {
@@ -216,5 +141,230 @@ public class WriteAndReadDataSet{
         return readValue;
     }
 
+    @Override
+    public void saveData(long time, float[] values) throws IOException {
+        OutputStream os = null;
+        try {
+            String filename = "data.txt";
+            os = new FileOutputStream(filename, true);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        int valueNumber = 0; // Counts the number of valid values contained inside of values[]
+
+        // This for loop tests if the entire values[] array is filled or not and counts the number of included values.
+        for(int i = 0; i < values.length; i++) {
+            try {
+                float temp = values[i];
+                valueNumber++;
+            } catch (NullPointerException ex) {
+                System.err.println("Array was not completely filled with values");
+                break;
+            }
+        }
+
+        /*
+        Then write the data set to data.txt in the following format:
+        1st: int -> number of temperature values in the set
+        2nd: long -> timestamp
+        3rd: float -> the temperature values themselves
+         */
+        this.writeInt(os, valueNumber);
+        this.writeLong(os, time);
+
+        for(int i = 0; i < valueNumber; i++) {
+            this.writeFloat(os, values[i]);
+        }
+
+        // Increase data size in size.txt by 1 to signify that a data set was added
+        int currentSize = this.getSize();
+        this.writeSize((currentSize+1));
+    }
+
+    @Override
+    public long getTimestamp(int dataset) throws IllegalArgumentException {
+
+        // checks if the parameter is valid
+        int currentSize = this.getSize();
+        if(dataset < 1 || dataset > currentSize) {
+            IllegalArgumentException ex = new IllegalArgumentException("Invalid data set exception");
+            throw  ex;
+        }
+
+        InputStream is = null;
+        try {
+            String filename = "data.txt";
+            is = new FileInputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        // skips to the correct place in the file if the dataset parameter was bigger than 1
+        for(int i = 1; i < dataset; i++) {
+            int nextSetValues = readInt(is);
+            for(int j = 0; j < (nextSetValues+2);j++) {
+                readFloat(is);
+            }
+        }
+
+        // reads the timestamp value from the desired set and returns it
+        readInt(is);
+        return readLong(is);
+    }
+
+    @Override
+    public float[] getValues(int dataset) throws IllegalArgumentException {
+
+        // checks if the parameter is valid
+        int currentSize = this.getSize();
+        if(dataset < 1 || dataset > currentSize) {
+            IllegalArgumentException ex = new IllegalArgumentException("Invalid data set exception");
+            throw  ex;
+        }
+
+        InputStream is = null;
+        try {
+            String filename = "data.txt";
+            is = new FileInputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        // skips to the correct place in the file if the dataset parameter was bigger than 1
+        for(int i = 1; i < dataset; i++) {
+            int nextSetValues = readInt(is);
+            for(int j = 0; j < (nextSetValues+2);j++) {
+                readFloat(is);
+            }
+        }
+
+        // writes the temperature values in the set to an array and returns it
+        int numberOfValues = readInt(is);
+        readLong(is);
+        float[] data = new float[numberOfValues];
+
+        for(int i = 0; i < numberOfValues; i++) {
+            data[i] = readFloat(is);
+        }
+
+        return data;
+    }
+
+    @Override
+    public int getSize() {
+        InputStream is = null;
+        try {
+            String filename = "size.txt";
+            is = new FileInputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+        // just returns the value written to size.txt
+        return this.readInt(is);
+    }
+
+    @Override
+    public void writeSize(int size) throws IllegalArgumentException {
+
+        // checks if the parameter is valid
+        if(size < 0) {
+            IllegalArgumentException ex = new IllegalArgumentException("Negative data set size error");
+            throw ex;
+        }
+        OutputStream os = null;
+        try {
+            String filename = "size.txt";
+            os = new FileOutputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        // just writes the parameter to size.txt
+        this.writeInt(os,size);
+    }
+
+    @Override
+    public void printAllData() {
+
+        InputStream isSize = null;
+        try {
+            String filename = "size.txt";
+            isSize = new FileInputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        int currentSize = readInt(isSize); // saves the amount of total data sets
+
+        InputStream isData = null;
+        try {
+            String filename = "data.txt";
+            isData = new FileInputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        // goes through the entire file and prints all relevant information
+        for(int i = 0; i < currentSize; i++) {
+            System.out.println("Data set "+(i+1)+":");
+            int nextSetValues = readInt(isData);
+            System.out.println("Timestamp: "+readLong(isData));
+            for(int j = 0; j < nextSetValues; j++) {
+                System.out.println("Value "+(j+1)+": "+readFloat(isData));
+            }
+            System.out.println("");
+        }
+    }
+
+    @Override
+    public void deleteAllData() {
+
+        //Empties both data.txt and size.txt of all data / creates them if they do not exist yet
+        OutputStream os = null;
+        try {
+            String filename = "data.txt";
+            os = new FileOutputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+        OutputStream os2 = null;
+        try {
+            String filename = "size.txt";
+            os2 = new FileOutputStream(filename);
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("couldn’t open file - fatal");
+            System.exit(0);
+
+        }
+
+        // initializes size.txt with the value 0
+        writeInt(os2,0);
+    }
 }
 
